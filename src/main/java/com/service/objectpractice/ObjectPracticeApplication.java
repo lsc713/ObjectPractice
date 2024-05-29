48dfd1f4524c5d9710e8b30ccff6195f266a35cb
lsc713 <lsc7134@naver.com>
2024-05-29 22:53:47 +0900

    InitCommit

---
+++ src/main/java/com/service/objectpractice/ObjectPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.service.objectpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectPracticeApplication.class, args);
+    }
+
+}
