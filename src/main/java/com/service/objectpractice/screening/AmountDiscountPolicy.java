deba5ff12565be964d6c55e2d0f976dcecf4d356
lsc713 <lsc7134@naver.com>
2024-05-30 23:18:46 +0900

    Object Chap 2 추상클래스는 상속을 통해 공유되는 메서드내부 공유

---
+++ src/main/java/com/service/objectpractice/screening/AmountDiscountPolicy.java
@@ -0,0 +1,16 @@
+package com.service.objectpractice.screening;
+
+public class AmountDiscountPolicy extends DiscountPolicy{
+
+    protected Money discountAmount;
+
+    public AmountDiscountPolicy(Money discountAmount, DiscountCondition... conditions) {
+        super(conditions);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return discountAmount;
+    }
+}
