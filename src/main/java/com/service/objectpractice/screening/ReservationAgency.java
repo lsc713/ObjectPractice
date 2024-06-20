7d15e30b7c10a87552478af6f79a4bbdab6997f1
lsc713 <lsc7134@naver.com>
2024-06-20 16:52:00 +0900

    Object Chap 4 데이터 중심의 클래스설계 캡슐화 및 응집도 강화

--- src/main/java/com/service/objectpractice/screening/ReservationAgency.java
+++ src/main/java/com/service/objectpractice/screening/ReservationAgency.java
@@ -8,43 +8,9 @@ public class ReservationAgency {
 
     public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
 
-        Movie movie = screening.getMovie();
-        boolean discountable = false;
-        for (DiscountCondition condition : movie.getDiscountConditions()) {
-            if (condition.getType() == DiscountConditionType.PERIOD) {
-                discountable =
-                    screening.getWhenScreened().getDayOfWeek().equals(condition.getDayOfWeek()) &&
-                        condition.getStartTime()
-                            .compareTo(
-                                ChronoLocalDateTime.from(screening.getWhenScreened().toLocalTime())) <= 0 &&
-                        condition.getEndTime().compareTo(
-                            ChronoLocalDateTime.from(screening.getWhenScreened().toLocalTime()))
-                            >= 0;
-            } else {
-                discountable = condition.getSequence() == screening.getSequence();
-            }
-            if (discountable) {
-                break;
-            }
-        }
-
-        Money fee;
-        if (discountable) {
-            Money discountAmount = Money.zero;
-            switch (movie.getMovieType()) {
-                case AMOUNT_DISCOUNT:
-                    discountAmount = movie.getDiscountAmount();
-                    break;
-                case PERCENT_DISCOUNT:
-                    discountAmount = movie.getFee().times(movie.getDiscountPercent());
-                    break;
-            }
-
-            fee = movie.getFee().minus(discountAmount);
-        } else {
-            fee = movie.getFee();
-        }
+        Money fee = screening.calculateFee(audienceCount);
         return new Reservation(customer, screening, fee, audienceCount);
+
     }
 
 }
