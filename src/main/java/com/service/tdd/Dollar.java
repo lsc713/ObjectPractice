52b66e4d5e0d70040f5010ff75418ec97debf578
lsc713 <lsc7134@naver.com>
2025-01-18 11:22:02 +0900

    practice tdd example

---
+++ src/main/java/com/service/tdd/Dollar.java
@@ -0,0 +1,14 @@
+package com.service.tdd;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public Dollar times(int multiplier) {
+        return new Dollar(this.amount*multiplier);
+    }
+}
---
+++ src/test/java/com/service/tdd/MoneyTest.java
@@ -0,0 +1,18 @@
+package com.service.tdd;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+    @Test
+    void testMultiplication(){
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10,product.amount);
+        product = five.times(3);
+        assertEquals(15,product.amount);
+    }
+}
