b850023e6c4438f0b898be29fd4771b3716237b1
lsc713 <lsc7134@naver.com>
2025-01-18 11:27:55 +0900

    practice tdd example equals & hashCode & private encapsulation

--- src/main/java/com/service/tdd/Dollar.java
+++ src/main/java/com/service/tdd/Dollar.java
@@ -1,8 +1,10 @@
 package com.service.tdd;
 
+import java.util.Objects;
+
 public class Dollar {
 
-    int amount;
+    private int amount;
 
     public Dollar(int amount) {
         this.amount = amount;
@@ -10,5 +12,15 @@ public class Dollar {
 
     public Dollar times(int multiplier) {
         return new Dollar(this.amount*multiplier);
+    }
+
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        Dollar dollar = (Dollar) object;
+        return this.amount == dollar.amount;
+    }
+
+    public int hashCode(){
+        return Objects.hash(amount);
     }
 }
