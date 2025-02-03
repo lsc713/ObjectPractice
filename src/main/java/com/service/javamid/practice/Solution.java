da06ea5e584305c635f5bb89c348edfd0ffacd97
lsc713 <lsc7134@naver.com>
2025-02-03 23:58:35 +0900

    solve

---
+++ src/main/java/com/service/javamid/practice/Solution.java
@@ -0,0 +1,29 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int bit = (1<<10)-1;
+            int result = n;
+            int temp = 0;
+            while(bit != temp){
+                char[] arr = String.valueOf(result).toCharArray();
+                for(int i = 0 ; i < arr.length ; i++){
+                    temp = temp | (1 << (arr[i] - '0'));
+                }
+                result += n;
+            }
+            sb.append("#"+i+ " "+(result-n)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
