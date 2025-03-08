21d368d5a18e2b3b0e4454f9d2be51da16edb775
lsc713 <lsc7134@naver.com>
2025-03-08 23:53:52 +0900

    solve

--- src/main/java/com/service/Solution2.java
+++ src/main/java/com/service/Solution2.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution {
+public class Solution2 {
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- src/main/java/com/service/javamid/practice/Solution.java
+++ src/main/java/com/service/javamid/practice/Solution.java
@@ -1,82 +1,37 @@
+package com.service.javamid.practice;
+
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
-public class Solution {
-    static int N;
-    static char[][] map;
-    static boolean[][] visited;
-    static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};  // 8방향
-    static int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    public static void main(String[] args) throws Exception {
+class Solution
+{
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        for(int tc = 1; tc <= T; tc++) {
-            N = Integer.parseInt(br.readLine());
-            map = new char[N][N];
-            visited = new boolean[N][N];
+        for (int t = 1 ; t <= T ; t++){
             int result = 0;
-
-            // 입력 (한 줄씩 문자열로 받음)
-            for(int i = 0; i < N; i++) {
-                String line = br.readLine();
-                for(int j = 0; j < N; j++) {
-                    map[i][j] = line.charAt(j);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            Set one = new HashSet();
+            for (int i = 0; i < n; i++){
+                one.add(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < m; i++){
+                if (one.contains(st.nextToken())){
+                    result++;
                 }
             }
-
-            for(int i = 0; i < N; i++) {
-                for(int j = 0; j < N; j++) {
-                    if(!visited[i][j] && map[i][j] == '.' && countMines(i, j) == 0) {
-                        dfs(i, j);
-                        result++;
-                    }
-                }
-            }
-
-            for(int i = 0; i < N; i++) {
-                for(int j = 0; j < N; j++) {
-                    if(!visited[i][j] && map[i][j] == '.') {
-                        result++;
-                    }
-                }
-            }
-
-            sb.append("#").append(tc).append(" ").append(result).append("\n");
+            System.out.println("#"+t+" "+result);
         }
-        System.out.print(sb);
-    }
-
-    static int countMines(int x, int y) {
-        int count = 0;
-        for(int i = 0; i < 8; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(isValid(nx, ny) && map[nx][ny] == '*') {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    static void dfs(int x, int y) {
-        visited[x][y] = true;
-
-        if(countMines(x, y) == 0) {
-            for(int i = 0; i < 8; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(isValid(nx, ny) && !visited[nx][ny] && map[nx][ny] == '.') {
-                    dfs(nx, ny);
-                }
-            }
-        }
-    }
-
-    static boolean isValid(int x, int y) {
-        return x >= 0 && x < N && y >= 0 && y < N;
     }
 }
