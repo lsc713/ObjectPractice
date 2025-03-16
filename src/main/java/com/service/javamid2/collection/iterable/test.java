15871e09a6bca4540584a1dbb9b85d30c6196bd5
lsc713 <lsc7134@naver.com>
2025-03-16 23:59:15 +0900

    solve

--- src/main/java/com/service/javamid2/collection/iterable/test.java
+++ src/main/java/com/service/javamid2/collection/iterable/test.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Solution {
+class test {
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
